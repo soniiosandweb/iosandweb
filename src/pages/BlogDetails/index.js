b663189a501689748428ac882f255b4a907ec45e
shreyanshi-123 <shreyanshi@iosandweb.net>
2025-07-08 09:39:31 +0530

    commit

--- src/pages/BlogDetails/index.js
+++ src/pages/BlogDetails/index.js
@@ -6,7 +6,7 @@ import { Row, Col, Container, Spinner } from "react-bootstrap";
 import SEO from "../../components/SEO";
 const defaultImage = `${process.env.REACT_APP_API_URL}/assests/images/placeholder-image.webp`;
 
-function BlogDetails(){
+function BlogDetails() {
     const url = useParams().url;
     const location = useLocation();
     const [blogId, setBlogId] = useState(null);
@@ -29,33 +29,33 @@ function BlogDetails(){
             url: `${process.env.REACT_APP_API_URL}/api/blog-details.php`,
             data: JSON.stringify({
                 post_url: url
-                }),
+            }),
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
         })
-        .then(res => {
-            for(var i = 0; i < res.data.length; i++) {
-                var obj = res.data[i];
-                if(obj.url === url){
-                    setBlogId(obj.id);
-                    setTitle(obj.title);
-                    setImage(obj.image);
-                    setDate(obj.date);
-                    setContent(obj.content);
-                    setSeoTitle(obj.seo_title);
-                    setSeoDescription(obj.seo_description);
-                    setSeoKeyword(obj.seo_keywords);
+            .then(res => {
+                for (var i = 0; i < res.data.length; i++) {
+                    var obj = res.data[i];
+                    if (obj.url === url) {
+                        setBlogId(obj.id);
+                        setTitle(obj.title);
+                        setImage(obj.image);
+                        setDate(obj.date);
+                        setContent(obj.content);
+                        setSeoTitle(obj.seo_title);
+                        setSeoDescription(obj.seo_description);
+                        setSeoKeyword(obj.seo_keywords);
+                    }
                 }
-            }
-        })
-        .catch(() => {
-            console.log('Error')
-        })
-        .finally(() => {
-            setLoading(false);
-        })
+            })
+            .catch(() => {
+                console.log('Error')
+            })
+            .finally(() => {
+                setLoading(false);
+            })
 
         // Related Post
-        if(blogId && blogId !== null){
+        if (blogId && blogId !== null) {
             axios({
                 method: "post",
                 url: `${process.env.REACT_APP_API_URL}/api/related-blog.php`,
@@ -64,70 +64,81 @@ function BlogDetails(){
                 }),
                 headers: { "Content-Type": "application/x-www-form-urlencoded" },
             })
-            .then(res => {
-                setRelatedPost(res.data);
-            })
-            .catch(() => {
-                console.log('Error')
-            })
-            .finally(() => {
-                setRelatedLoading(false);
-            })
+                .then(res => {
+                    setRelatedPost(res.data);
+                })
+                .catch(() => {
+                    console.log('Error')
+                })
+                .finally(() => {
+                    setRelatedLoading(false);
+                })
         }
-        
+
 
     }, [url, blogId]);
 
-    return(
+    return (
         <>
 
             {
                 loading ?
-                <>
-                    <div className="blog-detail-content section-padding">
-                        <Container>
-                            <Row>
-                                <Col>
-                                    <div className="blog_spinner text-center">
-                                        <Spinner as="span" animation="border" size="xl" role="status" aria-hidden="true" />
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </div> 
-                </>
-                :
-                <>
-                    <SEO
-                        title={seoTitle}
-                        description={seoDescription}
-                        name={"IosAndWeb Technologies"}
-                        keywords={seoKeyword}
-                        canonicalUrl={`${process.env.REACT_APP_API_URL}${location.pathname}`}
-                    />
+                    <>
+                        <div className="blog-detail-content section-padding">
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <div className="blog_spinner text-center">
+                                            <Spinner as="span" animation="border" size="xl" role="status" aria-hidden="true" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <SEO
+                            title={seoTitle}
+                            description={seoDescription}
+                            name={"IosAndWeb Technologies"}
+                            keywords={seoKeyword}
+                            canonicalUrl={`${process.env.REACT_APP_API_URL}${location.pathname}`}
+                        />
 
-                    <div className="blog-detail-page section-padding">
-                        <img src={image ? `${process.env.REACT_APP_BLOG_API_URL}/wp-content/uploads/${image}` : defaultImage} alt={title} className="blog-details-image" />
-                        <div className="blog-bg"></div>
-                        <Container>
-                            <Row>
-                                <Col lg={10} className="m-auto">
-                                    <h1>{title}</h1>
-                                    <p className="paragraph text-white text-center">{date}</p>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </div>
-                    <div className="blog-detail-content section-padding">
-                        <Container>
-                            <Row>
-                                <Col>
-                                    <div className="blog-details-content-div" dangerouslySetInnerHTML={ { __html: content } }></div>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </div> 
-                </>
+                        <div className="blog-detail-page section-padding">
+
+                            <div className="blog-bg"></div>
+                            <Container>
+                                <Row>
+                                    <Col lg={10} className="m-auto">
+                                        <h1>{title}</h1>
+                                        <p className="paragraph  text-center">{date}</p>
+                                    </Col>
+                                </Row>
+                            </Container>
+
+                        </div>
+                        <div className="thumbnail-img">
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <img src={image ? `${process.env.REACT_APP_BLOG_API_URL}/wp-content/uploads/${image}` : defaultImage} alt={title} className="blog-details-image border" />
+                                    </Col>
+                                </Row>
+                            </Container>
+
+                        </div>
+                        <div className="blog-detail-content blog-content-space">
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <div className="blog-details-content-div" dangerouslySetInnerHTML={{ __html: content }}></div>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </div>
+                    </>
             }
 
             <div className="blog-page section-padding no-top-padding">
@@ -140,35 +151,35 @@ function BlogDetails(){
                         </Col>
                     </Row>
                     <Row className="related-blogs-list">
-                        
+
                         {
                             relatedLoading ?
-                            <>
-                                <div className="blog_spinner text-center">
-                                    <Spinner as="span" animation="border" size="xl" role="status" aria-hidden="true" />
-                                </div>
-                            </>
-                            : 
-                            <>
-                                {relatedPost && relatedPost.map((item, index) => 
-                            
-                                    <Col md={6} xl={3} className="blog-col" key={item.id}>
-                                        <div className="blog-list-item related-blog-item">
-                                            <a key={index} href={"/blog/"+item.url}>
-                                                <img src={item.image ? `${process.env.REACT_APP_BLOG_API_URL}/wp-content/uploads/${item.image}` : defaultImage} className="blog-image" alt={item.title} />
-                                            </a>
-                                            <div className="blog-detail">
-                                                <p><span className="blog-date">{item.date}</span></p>
-                                                <a key={index} href={"/blog/"+item.url}>
-                                                    <h4>{item.title}</h4>
+                                <>
+                                    <div className="blog_spinner text-center">
+                                        <Spinner as="span" animation="border" size="xl" role="status" aria-hidden="true" />
+                                    </div>
+                                </>
+                                :
+                                <>
+                                    {relatedPost && relatedPost.map((item, index) =>
+
+                                        <Col md={6} xl={3} className="blog-col" key={item.id}>
+                                            <div className="blog-list-item related-blog-item">
+                                                <a key={index} href={"/blog/" + item.url}>
+                                                    <img src={item.image ? `${process.env.REACT_APP_BLOG_API_URL}/wp-content/uploads/${item.image}` : defaultImage} className="blog-image" alt={item.title} />
                                                 </a>
+                                                <div className="blog-detail">
+                                                    <p><span className="blog-date">{item.date}</span></p>
+                                                    <a key={index} href={"/blog/" + item.url}>
+                                                        <h4>{item.title}</h4>
+                                                    </a>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                )}
-                            </>
+                                        </Col>
+                                    )}
+                                </>
                         }
-                        
+
 
                     </Row>
                 </Container>
